4207d7cf65d1e4ea80f527e4fd54bb3586b2cde0
Andrew Creally <creallya@gmail.com>
2020-06-18 22:07:03 -0400

    Linear time solution. Add more test cases.

--- src/h_index_ii/solution.py
+++ src/h_index_ii/solution.py
@@ -7,11 +7,7 @@ class Solution:
         if not citations:
             return 0
 
-        return self.compute(0, len(citations) - 1, citations)
-
-    def compute(self, start: int, end: int, citations: List[int]) -> int:
-        if start == end:
-            return min(citations[end], len(citations) - end)
-        else:
-            mid = (start + end) // 2
-            return max(self.compute(start, mid, citations), self.compute(mid + 1, end, citations))
+        h = min(1, citations[len(citations) - 1])
+        for i in range(len(citations) - 2, -1, -1):
+            h = max(min(citations[i], len(citations) - i), h)
+        return h
--- tests/test_h_index_ii.py
+++ tests/test_h_index_ii.py
@@ -15,14 +15,29 @@ class TestHIndexII(unittest.TestCase):
     def test_hIndex_citations_is_empty_returns_zero(self):
         self._assert_hIndex(0, [])
 
-    def test_hIndex_only_publications_is_cited_zero_times_returns_zero(self):
+    def test_hIndex_only_publication_is_cited_zero_times_returns_zero(self):
         self._assert_hIndex(0, [0])
 
-    def test_hIndex_only_publications_is_cited_more_than_one_time_returns_1(self):
+    def test_hIndex_multiple_publication_cited_zero_times_returns_zero(self):
+        self._assert_hIndex(0, [0, 0, 0])
+
+    def test_hIndex_only_publication_is_cited_more_than_one_time_returns_1(self):
         self._assert_hIndex(1, [1])
 
-    def test_hIndex_only_publications_is_cited_more_than_one_times_returns_1(self):
+    def test_hIndex_only_publication_is_cited_more_than_one_times_returns_1(self):
         self._assert_hIndex(1, [4])
+
+    def test_hIndex_two_publications_first_cited_once_returns_1(self):
+        self._assert_hIndex(1, [1,2])
+
+    def test_hIndex_two_publications_last_cited_once_returns_1(self):
+        self._assert_hIndex(1, [0,1])
+
+    def test_hIndex_two_publications_first_cited_twice_returns_2(self):
+        self._assert_hIndex(2, [2,3])
+
+    def test_hIndex_two_publications_first_cited_more_than_twice_returns_2(self):
+        self._assert_hIndex(2, [201,301])
 
     def test_hIndex_number_of_publications_less_than_min_number_of_citations_returns_number_of_citations(self):
         self._assert_hIndex(3, [5, 6, 7])
@@ -41,6 +56,9 @@ class TestHIndexII(unittest.TestCase):
 
     def test_hIndex_more_publications_with_at_least_number_of_citations_but_no_publications_with_less(self):
         self._assert_hIndex(3, [3, 3, 5, 8, 25])
+
+    def test_hIndex_multiple_citations_with_highest_h_index_greater_than_h(self):
+        self._assert_hIndex(4, [3, 3, 4, 4, 6, 6, 8, 10])
 
     def _assert_hIndex(self, expected: int, citations: List[int]) -> None:
         actual = self.solution.hIndex(citations)
